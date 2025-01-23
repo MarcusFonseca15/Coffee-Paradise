85f14b58584d988c1e381427b98ed35a86892175
Marcus <marcusfonseca15@gmail.com>
2025-01-22 22:46:59 -0300

    PAG INIT: Sessão de Filtros +  Carrossel (falha)

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("TESTE");
+const swiper = new Swiper('.card-wrapper', {
+    loop: true,
+  
+    // Pontos de Pagina
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-btn-next',
+      prevEl: '.swiper-btn-prev',
+    }
+
+  });
+  
